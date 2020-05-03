5ad86b214b601d9cb85d7228c933a83bff712b7c
Lukas Steiner <lukas.steiner@steinheilig.de>
2020-05-03 17:46:25 +0200

    add option to install plugins

--- foreman-server/entrypoint.sh
+++ foreman-server/entrypoint.sh
@@ -3,15 +3,20 @@
 export SEED_ADMIN_USER=${FOREMAN_ADMIN_USER:-admin}
 export SEED_ADMIN_PASSWORD=${FOREMAN_ADMIN_PASSWORD:-changeme}
 
+# replace environment variables in config files
 sed -i "s/##REDIS_URL##/${REDIS_URL//\//\\/}/g" /etc/foreman/settings.yaml
-
 sed -i "s/##POSTGRES_HOST##/$POSTGRES_HOST/g" /etc/foreman/database.yml
 sed -i "s/##POSTGRES_DATABASE##/$POSTGRES_DATABASE/g" /etc/foreman/database.yml
 sed -i "s/##POSTGRES_USERNAME##/$POSTGRES_USERNAME/g" /etc/foreman/database.yml
 sed -i "s/##POSTGRES_PASSWORD##/$POSTGRES_PASSWORD/g" /etc/foreman/database.yml
-
 sed -i "s/##ENCRYPTION_KEY##/$ENCRYPTION_KEY/g" /etc/foreman/encryption_key.rb
 
+# check for new CA certificates and import them
+if [ ! -z "$(ls -A /etc/pki/ca-trust/source/anchors)" ]; then
+    sudo update-ca-trust
+fi
+
+# migrate database
 /usr/share/foreman/extras/dbmigrate
 
 FOREMAN_ENV=production
